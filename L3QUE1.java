859ed582af40e44633872369a12c1c6164475651
andfe05 <andrefelipeoliveirad@gmail.com>
2025-09-22 21:06:13 -0300

    Add files via upload

---
+++ L3QUE1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class L3QUE1 {
+        public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+
+        for (int i = 15;  i <=100; i++){
+            int qua= i*i;
+            System.out.println(qua);
+        }
+        input.close();
+    }
+}
